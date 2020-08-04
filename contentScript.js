b666f964fd2e7b59098f90765abdee3946991875
thevvv36 <anonymalgerien@gmail.com>
2020-08-04 02:18:24 +0100

    write the contentscript

---
+++ contentScript.js
@@ -0,0 +1,13 @@
+console.log("content script running ...");
+const FIND = "the";
+const REPLACE_WITH = "aghiles";
+
+cont regex = new RegExpt(FIND, "gi");
+const content = document.querySelectorAll("p,li,span,h1,h2,h3");
+
+
+for(const snippet in contents){
+	snippet.textContent = snippet.textContent.replace(regex, REPLACE_WITH);
+}
+
+
