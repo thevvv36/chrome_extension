47f844c1700d2fd9a4fe0d5ce4d31a60714d239c
thevvv36 <anonymalgerien@gmail.com>
2020-08-05 02:28:58 +0100

    add event listenner

--- background.js
+++ background.js
@@ -1,9 +1,8 @@
 
 //iconRules contains all the rules for our extension
 //every rule is an object with two key : conditions and actions
-//
+
 const iconRules = [{
-console.log('background script ');
 	conditions:[
 		new chrome.declarativeContent.PageStateMatcher({
 			pargeUrl:{hostEquals:'medium'};
@@ -12,6 +11,18 @@ console.log('background script ');
 	actions:[new chrome.declarativeContent.ShowPageAction()]
 }];
 
-chrome.declarativeContent.inPageChanged.remvoeRules(undefined, function(){
+//remove all existing rules on the page when we change the page 
+//by passing undefined to chrome.declarativeContent.onPageChanged.removeRules
+
+chrome.declarativeContent.onPageChanged.remvoeRules(undefined, function(){
 	chrome.declarativeContent.onPageChanged.addRules(iconRules);
 });
+
+//the chrome.pageAction.onClicked api takes a callback which is fired 
+//whenever the extension icon in the browser toolbar is clicked
+
+chrome.pageAction.onClicked.addListener(function(){
+	chrome.tabs.qery({active:true, currentWindow:true},
+			function(tabs){
+				chrome.tabs.sendMessage(tabs[0].id, {action:"REPLACE_TEXT"});
+			});
