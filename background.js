c003a56e176769b9f1888663442bb3c1e4eeef85
thevvv36 <anonymalgerien@gmail.com>
2020-08-05 01:30:48 +0100

    background init

--- background.js
+++ background.js
@@ -1,2 +1,12 @@
-console.log('background script ');
+const iconRules = [{
+	conditions:[
+		new chrome.declarativeContent.PageStateMatcher({
+			pargeUrl:{hostEquals:'medium'};
+		})
+	],
+	actions:[new chrome.declarativeContent.ShowPageAction()]
+}];
 
+chrome.declarativeContent.inPageChanged.remvoeRules(undefined, function(){
+	chrome.declarativeContent.onPageChanged.addRules(iconRules);
+});
