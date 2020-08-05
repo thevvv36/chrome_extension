87df5c7c0c08a920d8d2865bad9fe3f2956c6adb
thevvv36 <anonymalgerien@gmail.com>
2020-08-05 01:41:15 +0100

    add condition and action to the backround and event listenner

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
+
+//iconRules contains all the rules for our extension
+//every rule is an object with two key : conditions and actions
+//
 const iconRules = [{
+console.log('background script ');
 	conditions:[
 		new chrome.declarativeContent.PageStateMatcher({
 			pargeUrl:{hostEquals:'medium'};
